18b6e71fc29a58da44bc84b3e234d7384ca08be3
Shang-Yi Yang <ilway25@gmail.com>
2024-08-27 14:29:49 +0800

    Adapt AVX's EF ot Neon

--- src/neon/echelon_form.h
+++ src/neon/echelon_form.h
@@ -1,138 +1,55 @@
-
 // SPDX-License-Identifier: Apache-2.0
 
-#ifndef ECHELON_FORM_H
-#define ECHELON_FORM_H
-
-#include <stdalign.h>
-#include <stdint.h>
+#include <arithmetic_common.h>
 #include <mem.h>
-#include <arithmetic.h>
+#include <arm_neon.h>
+#include <stdint.h>
+
 
 #define MAYO_MAX(x, y) (((x) > (y)) ? (x) : (y))
 #define MAYO_MIN(x, y) (((x) < (y)) ? (x) : (y))
 
-static inline unsigned char
-m_extract_element(const uint64_t *in, int index) {
-    const int leg = index / 16;
-    const int offset = index % 16;
+/* put matrix in row echelon form with ones on first nonzero entries in constant time*/
+static inline void EF(unsigned char *A, int _nrows, int _ncols) {
 
-    return (in[leg] >> (offset*4)) & 0xF;
-}
+    (void) _nrows;
+    (void) _ncols;
 
-static inline void
-ef_pack_m_vec(const unsigned char *in, uint64_t *out, int ncols) {
-    int i;
-    unsigned char *out8 = (unsigned char *)out;
-    for(i = 0; i+1 < ncols; i += 2){
-        out8[i/2]  = (in[i+0] << 0) | (in[i+1] << 4);
-    }
-    if (ncols % 2 == 1){
-        out8[i/2]  = (in[i+0] << 0);
-    }
-}
+    #define nrows M_MAX
+    #define ncols (K_MAX * O_MAX + 1)
 
-static inline void
-ef_unpack_m_vec(int legs, const uint64_t *in, unsigned char *out) {
-    const unsigned char *in8 = (const unsigned char *)in;
-    for(int i = 0; i < legs * 16; i += 2){
-        out[i]   = (in8[i/2]) & 0xF;
-        out[i+1] = (in8[i/2] >> 4);
-    }
-}
+    #define NEON_REGS_PER_ROW ((K_MAX * O_MAX + 1 + 15) / 16)
+    #define MAX_COLS (NEON_REGS_PER_ROW * 16)
 
+    uint8x16_t _pivot_row[NEON_REGS_PER_ROW];
+    uint8x16_t A_neon[NEON_REGS_PER_ROW* M_MAX];
 
-// put matrix in row echelon form with ones on first nonzero entries *in
-// constant time*
-static inline void EF(unsigned char *A, int nrows, int ncols) {
+    unsigned char* pivot_row_bytes = (unsigned char*) _pivot_row;
+    unsigned char* A_bytes = (unsigned char*) A_neon;
 
-    alignas (32) uint64_t _pivot_row[(K_MAX * O_MAX + 1 + 15) / 16];
-    alignas (32) uint64_t _pivot_row2[(K_MAX * O_MAX + 1 + 15) / 16];
-    alignas (32) uint64_t packed_A[((K_MAX * O_MAX + 1 + 15) / 16) * M_MAX];
-
-    int row_len = (ncols + 15) / 16;
-
-    // nibbleslice the matrix A
+    // load A in the tail of NEON registers
     for (int i = 0; i < nrows; i++) {
-        ef_pack_m_vec(A + i * ncols, packed_A + i * row_len, ncols);
+        for (int j = 0; j < ncols; j++)
+        {
+            A_bytes[i*MAX_COLS + (MAX_COLS - ncols) + j] = A[ i*ncols + j ];
+        }
     }
 
     // pivot row is secret, pivot col is not
-
     unsigned char inverse;
     int pivot_row = 0;
-    for (int pivot_col = 0; pivot_col < ncols; pivot_col++) {
-
-        int pivot_row_lower_bound = MAYO_MAX(0, pivot_col + nrows - ncols);
-        int pivot_row_upper_bound = MAYO_MIN(nrows - 1, pivot_col);
-        // the pivot row is guaranteed to be between these lower and upper bounds if
-        // A has full rank
-
-        // zero out pivot row
-        for (int i = 0; i < row_len; i++) {
-            _pivot_row[i] = 0;
-            _pivot_row2[i] = 0;
-        }
-
-        // try to get a pivot row in constant time
-        unsigned char pivot = 0;
-        uint64_t pivot_is_zero = -1;
-        for (int row = pivot_row_lower_bound;
-                row <= MAYO_MIN(nrows - 1, pivot_row_upper_bound + 32); row++) {
-
-            uint64_t is_pivot_row = ~ct_compare_64(row, pivot_row);
-            uint64_t below_pivot_row = ct_64_is_greater_than(row, pivot_row);
-
-            for (int j = 0; j < row_len; j++) {
-                _pivot_row[j] ^= (is_pivot_row | (below_pivot_row & pivot_is_zero)) &
-                                 packed_A[row * row_len + j];
-            }
-            pivot = m_extract_element(_pivot_row, pivot_col);
-            pivot_is_zero = ~ct_compare_64((int) pivot, 0);
-        }
-
-        // multiply pivot row by inverse of pivot
-        inverse = inverse_f(pivot);
-        vec_mul_add_u64(row_len, _pivot_row, inverse, _pivot_row2);
-
-        // conditionally write pivot row to the correct row, if there is a nonzero
-        // pivot
-        for (int row = pivot_row_lower_bound; row <= pivot_row_upper_bound; row++) {
-            uint64_t do_copy = ~ct_compare_64(row, pivot_row) & ~pivot_is_zero;
-            uint64_t do_not_copy = ~do_copy;
-            for (int col = 0; col < row_len; col++) {
-                packed_A[row * row_len + col] =
-                    (do_not_copy & packed_A[row * row_len + col]) +
-                    (do_copy & _pivot_row2[col]);
-            }
-        }
-
-        // eliminate entries below pivot
-        for (int row = pivot_row_lower_bound; row < nrows; row++) {
-            unsigned char below_pivot = (row > pivot_row);
-            unsigned char elt_to_elim = m_extract_element(packed_A + row * row_len, pivot_col);
-
-            vec_mul_add_u64(row_len, _pivot_row2, below_pivot * elt_to_elim,
-                                    packed_A + row * row_len);
-        }
-
-        pivot_row += (-(int32_t)(~pivot_is_zero));
+    int pivot_col = MAYO_MAX(MAX_COLS - ncols,0);
+    for (; pivot_col < MAX_COLS; pivot_col++) {
+        #include "echelon_form_loop.h"
     }
 
-    unsigned char temp[(O_MAX * K_MAX + 1 + 15)];
-
-    // unbitslice the matrix A
+    // write the matrix A back
     for (int i = 0; i < nrows; i++) {
-        ef_unpack_m_vec(row_len, packed_A + i * row_len, temp);
         for (int j = 0; j < ncols; j++) {
-            A[i * ncols + j] = temp[j];
+            A[i * ncols + j] = A_bytes[i*NEON_REGS_PER_ROW*16 + (MAX_COLS - ncols) + j];
         }
     }
-
-    mayo_secure_clear(temp, K_MAX * O_MAX + 1 + 15);
-    mayo_secure_clear(_pivot_row, (K_MAX * O_MAX + 1 + 15) / 16 * 8);
-    mayo_secure_clear(_pivot_row2, (K_MAX * O_MAX + 1 + 15) / 16 * 8);
-    mayo_secure_clear(packed_A, ((K_MAX * O_MAX + 1 + 15) / 16) * M_MAX * 8);
+    mayo_secure_clear(_pivot_row, NEON_REGS_PER_ROW * 16);
+    mayo_secure_clear(A_neon, NEON_REGS_PER_ROW * 16 * nrows);
 }
 
-#endif
